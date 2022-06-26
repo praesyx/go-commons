edde6a8601a63d4ce0669abb4f5ef1ee65d73583
Pavel Ustimenko <steve.jsfolio@gmail.com>
2022-06-26 17:21:24 +0500

    first commit

---
+++ pointer.go
@@ -0,0 +1,41 @@
+package commons
+
+func String(val string) *string {
+	return &val
+}
+
+func Int(n int) *int {
+	return &n
+}
+
+func Int8(n int8) *int8 {
+	return &n
+}
+
+func Int16(n int16) *int16 {
+	return &n
+}
+
+func Int32(n int32) *int32 {
+	return &n
+}
+
+func Int64(n int64) *int64 {
+	return &n
+}
+
+func Uint8(n uint8) *uint8 {
+	return &n
+}
+
+func Uint16(n uint16) *uint16 {
+	return &n
+}
+
+func Uint32(n uint32) *uint32 {
+	return &n
+}
+
+func Uint64(n uint64) *uint64 {
+	return &n
+}
