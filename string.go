c3c86f10005d7e52c8f3f4aaa3bccd5061a5d729
Praesyx <steve.jsfolio@gmail.com>
2022-11-02 01:48:41 +0100

    добавлен обрез строки по длине

---
+++ string.go
@@ -0,0 +1,21 @@
+package commons
+
+import "unicode"
+
+func TripByLen(str string, max int) string {
+	lastSpaceIx := -1
+	n := 0
+	for i, r := range str {
+		if unicode.IsSpace(r) {
+			lastSpaceIx = i
+		}
+		n++
+		if n >= max {
+			if lastSpaceIx != -1 {
+				return str[:lastSpaceIx] + "..."
+			}
+		}
+	}
+
+	return str
+}
