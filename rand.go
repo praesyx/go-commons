93b3c23e99e66add1c822a7ccc9bd4d2c616da75
Praesyx <steve.jsfolio@gmail.com>
2023-01-13 05:00:01 +0100

    random int from range

--- rand.go
+++ rand.go
@@ -6,6 +6,16 @@ import (
 )
 
 func RandomIntFromRange(min, max int) int {
+	if min < 0 {
+		min = 0
+	}
+	if max < min {
+		max = min
+	}
+	n := max - min + 1
+	if n < 0 {
+		n = 0
+	}
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min+1) + min
+	return rand.Intn(n) + min
 }
